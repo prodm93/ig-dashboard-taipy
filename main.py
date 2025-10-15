3f65fe364fcb78eabb002a30bb9fc14537afa9ad
Prodhi M <105621504+prodm93@users.noreply.github.com>
2025-10-15 03:59:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from taipy.gui import Gui
 from components.sidebar import sidebar
-from pages import engagement_dashboard, content_efficiency_dashboard, sentiment_dashboard
+from pages import engagement_dashboard, content_efficiency_dashboard, semantics_dashboard
 
 # Define page routes
 pages = {
     "/": sidebar,
     "Engagement Dashboard": engagement_dashboard.layout,
     "Content Efficiency": content_efficiency_dashboard.layout,
-    "Semantics & Sentiment": sentiment_dashboard.layout,
+    "Semantics & Sentiment": semantics_dashboard.layout,
 }
 
 # Launch the GUI
